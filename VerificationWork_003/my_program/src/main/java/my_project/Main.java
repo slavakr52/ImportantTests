e87b4000d048cdf0155bda112e781cc9eb665cbc
slavakr52 <slavakr52@mail.ru>
2023-09-21 14:56:19 +0300

    update: in progress

--- VerificationWork_003/my_program/src/main/java/my_project/Main.java
+++ VerificationWork_003/my_program/src/main/java/my_project/Main.java
@@ -22,15 +22,50 @@ public class Main {
                     + "\n3."
                     + "\n4. Выход");
 
-            System.out.print("Введите пункт меню: ");
+            System.out.print("Выберите пункт меню: ");
             try {
                 int userChoose = scanner.nextInt();
 
+                // выбор пункта меню
                 switch (userChoose) {
                     case 1:
+
+                        System.out.print("Введите кличку животного: ");
+                        String name = scanner.nextLine();
+                        System.out.print("Введите кличку животного: ");
+                        String birth_date = scanner.nextLine();
+                        System.out.print("Введите кличку животного: ");
+                        String command = scanner.nextLine();
+                        
+                        // выбор типа животного
                         System.out.println("\nВыберите тип животного:"
                                 + "\n1. Домашнее животное"
                                 + "\n2. Вьючное животное");
+                        int userChoose2 = scanner.nextInt();
+                        switch (userChoose2) {
+                            //домашнее животное
+                            case 1:
+                                String type1 = "Pet";
+                                System.out.println("\nКакое домашнее животное вы хотите добавить?"
+                                        + "\n1. Собака"
+                                        + "\n2. Кошка"
+                                        + "\n3. Хомяк");
+                                int userChoose3 = scanner.nextInt();
+                                switch (userChoose3) {
+                                    //собака
+                                    case 1:
+                                    Dog dog = new Dog (type1, "1", name, birth_date, command);
+
+                                }
+                            //вьючное животное
+                            case 2:
+                                String type2 = "Pack Animal";
+                                System.out.println("\nКакое вьючное животное вы хотите добавить?"
+                                        + "\n1. Лошадь"
+                                        + "\n2. Верблюд"
+                                        + "\n3. Осёл");
+                        }
+
                     case 2:
                         break;
                     case 3:
